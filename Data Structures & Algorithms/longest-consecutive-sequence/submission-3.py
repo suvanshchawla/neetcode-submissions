2e4eb4e6ab6c8a1cbd780b00e1cd2083eaa6be86
Suvansh Chawla <suvanshchawla83@gmail.com>
2026-09-16 16:02:31 -0400

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.py
@@ -0,0 +1,33 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        hashset = defaultdict(set)
+        
+        if not len(nums):
+            return len(nums)
+
+        maxlen = 0
+        curr = 1
+        
+        for num in nums:
+            hashset[num] = num
+        
+        for num in nums:
+            curr = 1
+            if num - 1 not in hashset:
+                # this is the beginning of the sequence
+                curr_num = num
+                while curr_num + 1 in hashset:
+                    curr+=1
+                    maxlen = max(curr, maxlen)
+                    curr_num = curr_num+1
+            
+        
+        return max(curr, maxlen)
+                
+
+
+        
+# we need to find the sequence, even if it is not consecutive in the array
+# simplest way is to create an enumeration and sort the array but that takes O(nlogn)
+# how to find the start of the sequence?
+# create a hashset and put all the numbers in it
